af18e23ac9e1deca6e8033c936e64e4f53b16068
Aastha <aastha2150@gmail.com>
2021-03-20 19:14:30 +1300

    some final changes from client

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,175 +2,175 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 class Footer extends React.Component {
-	render() {
-		return (
-			<div>
-				<footer class='footer-section'>
-					<div class='container'>
-						<div class='row'>
-							<div class='col-lg-3'>
-								<div class='footer-left'>
-									<div class='footer-logo'>
-										<img
-											src='img/logo_image.jpeg'
-											width='100'
-											height='50'
-											alt=''
-										/>
-										<img
-											src='img/logo_name.jpeg'
-											height='50'
-											width='120'
-											alt=''
-										/>
-									</div>
-									<ul>
-										<li>Address: 202,Udhyog vihar phase V,Gurgaon(Haryana)</li>
-										<li>Phone: +91 9958065877</li>
-										
-										<li>Email: modebyzara@gmail.com</li>
-									</ul>
-									<div class='footer-social'>
-										<a href='https://www.facebook.com/modeZara6/'>
-											<i class='fa fa-facebook'></i>
-										</a>
-										<a href='#'>
-											<i class='fa fa-instagram'></i>
-										</a>
-										<a href='#'>
-											<i class='fa fa-twitter'></i>
-										</a>
-									</div>
-								</div>
-							</div>
-							<div class='col-lg-2 offset-lg-1'>
-								<div class='footer-widget'>
-									<h5>Quick Links</h5>
-									<ul>
-										<li>
-											<NavLink
-												exact
-												className='login-panel'
-												activeClassName='is-active'
-												to='/Home'>
-												Home
-											</NavLink>
-										</li>
-										<li>
-											<NavLink
-												exact
-												className='login-panel'
-												activeClassName='is-active'
-												to='/ContactUs'>
-												Contact Us
-											</NavLink>
-										</li>
+ render() {
+  return (
+   <div>
+    <footer class="footer-section">
+     <div class="container">
+      <div class="row">
+       <div class="col-lg-3">
+        <div class="footer-left">
+         <div class="footer-logo">
+          <NavLink
+           exact
+           className="login-panel"
+           activeClassName="is-active"
+           to="/Home"
+          >
+           <img src="img/footer_icon.jfif" />
+           <img src="img/footer_logo.jfif" />
+          </NavLink>
+         </div>
+         <ul>
+          <li>Address: 202,Udhyog vihar phase V,Gurgaon(Haryana)</li>
+          <li>Phone: +91 9958065877</li>
 
-										<li>
-											<NavLink
-												exact
-												className='login-panel'
-												activeClassName='is-active'
-												to='/Login'>
-												Admin Login
-											</NavLink>
-										</li>
-									</ul>
-								</div>
-							</div>
-							<div class='col-lg-2'>
-								<div class='footer-widget'>
-									<h5>Our Collection </h5>
-									<ul>
-										<li>
-											<NavLink
-												exact
-												className='login-panel'
-												activeClassName='is-active'
-												to='/Kurta'>
-												Kurta
-											</NavLink>
-										</li>
-										<li>
-											<NavLink
-												exact
-												className='login-panel'
-												activeClassName='is-active'
-												to='/KurtaPlazo'>
-												Kurta Plazo set
-											</NavLink>
-										</li>
-										<li>
-											<NavLink
-												exact
-												className='login-panel'
-												activeClassName='is-active'
-												to='/ALineKurta'>
-												A Line Kurta
-											</NavLink>
-										</li>
-										<li>
-											<NavLink
-												exact
-												className='login-panel'
-												activeClassName='is-active'
-												to='/Dupatta'>
-												Kurta Plazo Dupatta set
-											</NavLink>
-										</li>
-									</ul>
-								</div>
-							</div>
-							<div class='col-lg-4'>
-								<div class='newslatter-item'>
-									<h5>Join Our Newsletter Now</h5>
-									<p>
-										Get E-mail updates about our latest shop and special offers.
-									</p>
-									<form action='#' class='subscribe-form'>
-										<input type='text' placeholder='Enter Your Mail' />
-										<button type='button'>Subscribe</button>
-									</form>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class='copyright-reserved'>
-						<div class='container'>
-							<div class='row'>
-								<div class='col-lg-12'>
-									<div class='copyright-text'>
-										Copyright &copy;
-										<script>
-											document.write(new Date().getFullYear());
-										</script>{" "}
-										All rights reserved | This template is made with{" "}
-										<i class='fa fa-heart-o' aria-hidden='true'></i> by{" "}
-										<a href='https://colorlib.com' target='_blank'>
-											Colorlib
-										</a>
-									</div>
-									<div class='payment-pic'>
-										<img src='img/payment-method.png' alt='' />
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</footer>
-				<script src='../js/jquery-3.3.1.min.js'></script>
-				<script src='../js/bootstrap.min.js'></script>
-				<script src='../js/jquery-ui.min.js'></script>
-				<script src='../js/jquery.countdown.min.js'></script>
-				<script src='../js/jquery.nice-select.min.js'></script>
-				<script src='../js/jquery.zoom.min.js'></script>
-				<script src='../js/jquery.dd.min.js'></script>
-				<script src='../js/jquery.slicknav.js'></script>
-				<script src='../js/owl.carousel.min.js'></script>
-				<script src='../js/main.js'></script>
-			</div>
-		);
-	}
+          <li>Email: modebyzara@gmail.com</li>
+         </ul>
+         <div class="footer-social">
+          <a href="https://www.facebook.com/modeZara6/">
+           <i class="fa fa-facebook"></i>
+          </a>
+          <a href="#">
+           <i class="fa fa-instagram"></i>
+          </a>
+          <a href="#">
+           <i class="fa fa-twitter"></i>
+          </a>
+         </div>
+        </div>
+       </div>
+       <div class="col-lg-2 offset-lg-1">
+        <div class="footer-widget">
+         <h5>Quick Links</h5>
+         <ul>
+          <li>
+           <NavLink
+            exact
+            className="login-panel"
+            activeClassName="is-active"
+            to="/Home"
+           >
+            Home
+           </NavLink>
+          </li>
+          <li>
+           <NavLink
+            exact
+            className="login-panel"
+            activeClassName="is-active"
+            to="/ContactUs"
+           >
+            Contact Us
+           </NavLink>
+          </li>
+
+          <li>
+           <NavLink
+            exact
+            className="login-panel"
+            activeClassName="is-active"
+            to="/Login"
+           >
+            Admin Login
+           </NavLink>
+          </li>
+         </ul>
+        </div>
+       </div>
+       <div class="col-lg-2">
+        <div class="footer-widget">
+         <h5>Our Collection </h5>
+         <ul>
+          <li>
+           <NavLink
+            exact
+            className="login-panel"
+            activeClassName="is-active"
+            to="/Kurta"
+           >
+            Kurta
+           </NavLink>
+          </li>
+          <li>
+           <NavLink
+            exact
+            className="login-panel"
+            activeClassName="is-active"
+            to="/KurtaPlazo"
+           >
+            Kurta Plazo set
+           </NavLink>
+          </li>
+          <li>
+           <NavLink
+            exact
+            className="login-panel"
+            activeClassName="is-active"
+            to="/ALineKurta"
+           >
+            A Line Kurta
+           </NavLink>
+          </li>
+          <li>
+           <NavLink
+            exact
+            className="login-panel"
+            activeClassName="is-active"
+            to="/Dupatta"
+           >
+            Kurta Plazo Dupatta set
+           </NavLink>
+          </li>
+         </ul>
+        </div>
+       </div>
+       <div class="col-lg-4">
+        <div class="newslatter-item">
+         <h5>Join Our Newsletter Now</h5>
+         <p>Get E-mail updates about our latest shop and special offers.</p>
+         <form action="#" class="subscribe-form">
+          <input type="text" placeholder="Enter Your Mail" />
+          <button type="button">Subscribe</button>
+         </form>
+        </div>
+       </div>
+      </div>
+     </div>
+     <div class="copyright-reserved">
+      <div class="container">
+       <div class="row">
+        <div class="col-lg-12">
+         <div class="copyright-text">
+          Copyright &copy;
+          <script>document.write(new Date().getFullYear());</script> All rights
+          reserved | This template is made with{" "}
+          <i class="fa fa-heart-o" aria-hidden="true"></i> by{" "}
+          <a href="https://colorlib.com" target="_blank">
+           Colorlib
+          </a>
+         </div>
+         <div class="payment-pic">
+          <img src="img/payment-method.png" alt="" />
+         </div>
+        </div>
+       </div>
+      </div>
+     </div>
+    </footer>
+    <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/jquery-ui.min.js"></script>
+    <script src="../js/jquery.countdown.min.js"></script>
+    <script src="../js/jquery.nice-select.min.js"></script>
+    <script src="../js/jquery.zoom.min.js"></script>
+    <script src="../js/jquery.dd.min.js"></script>
+    <script src="../js/jquery.slicknav.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/main.js"></script>
+   </div>
+  );
+ }
 }
 
 export default Footer;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,171 +1,186 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Dropdown from "react-bootstrap/Dropdown"
+import Dropdown from "react-bootstrap/Dropdown";
 
 class Header extends React.Component {
-  constructor() {
-    super();
+ constructor() {
+  super();
 
-    this.state = {
-      home: true,
-      cat1: false,
-      cat2: false,
-      cat3: false,
-      cat4: false,
-      contact: false,
-      selectedOption: "",
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = (e) => {
-    e.preventDefault();
-
-    switch (e.target.id) {
-      case "home":
-        this.setState({
-          home: true,
-          cat1: false,
-          cat2: false,
-          cat3: false,
-          cat4: false,
-          contact: false,
-        });
-        break;
-      case "cat1":
-        this.setState({
-          home: false,
-          cat1: true,
-          cat2: false,
-          cat3: false,
-          cat4: false,
-          contact: false,
-        });
-        break;
-      case "cat2":
-        this.setState({
-          home: false,
-          cat1: false,
-          cat2: true,
-          cat3: false,
-          cat4: false,
-          contact: false,
-        });
-        break;
-      case "cat3":
-        this.setState({
-          home: false,
-          cat1: false,
-          cat2: false,
-          cat3: true,
-          cat4: false,
-          contact: false,
-        });
-        break;
-      case "cat4":
-        this.setState({
-          home: false,
-          cat1: false,
-          cat2: false,
-          cat3: false,
-          cat4: true,
-          contact: false,
-        });
-        break;
-      case "contact":
-        this.setState({
-          home: false,
-          cat1: false,
-          cat2: false,
-          cat3: false,
-          cat4: false,
-          contact: true,
-        });
-        break;
-    }
-
-    //e.target.className="navvar-item active is-active ";
-    //e.target.backgroundColor="#E7AB3C";
+  this.state = {
+   home: true,
+   cat1: false,
+   cat2: false,
+   cat3: false,
+   cat4: false,
+   contact: false,
+   selectedOption: "",
   };
+  this.handleClick = this.handleClick.bind(this);
+ }
 
-  categorySelected(e) {
-    e.preventDefault();
-    console.log(e);
+ handleClick = (e) => {
+  e.preventDefault();
+
+  switch (e.target.id) {
+   case "home":
     this.setState({
-      selectedOption: e.target.value,
+     home: true,
+     cat1: false,
+     cat2: false,
+     cat3: false,
+     cat4: false,
+     contact: false,
     });
+    break;
+   case "cat1":
+    this.setState({
+     home: false,
+     cat1: true,
+     cat2: false,
+     cat3: false,
+     cat4: false,
+     contact: false,
+    });
+    break;
+   case "cat2":
+    this.setState({
+     home: false,
+     cat1: false,
+     cat2: true,
+     cat3: false,
+     cat4: false,
+     contact: false,
+    });
+    break;
+   case "cat3":
+    this.setState({
+     home: false,
+     cat1: false,
+     cat2: false,
+     cat3: true,
+     cat4: false,
+     contact: false,
+    });
+    break;
+   case "cat4":
+    this.setState({
+     home: false,
+     cat1: false,
+     cat2: false,
+     cat3: false,
+     cat4: true,
+     contact: false,
+    });
+    break;
+   case "contact":
+    this.setState({
+     home: false,
+     cat1: false,
+     cat2: false,
+     cat3: false,
+     cat4: false,
+     contact: true,
+    });
+    break;
   }
 
-  render() {
-    const data = [
-      "Kurta",
-      "Kurta Plazo Set",
-      "A Line Kurta",
-      "Kurta Plazo Dupatta Set",
-    ];
-    let cats = data.map((ex) => {
-      return (
-        <option value={ex} key={ex} onClick={this.handleDropdown}>
-          {" "}
-          {ex}
-        </option>
-      );
-    });
-    return (
-      <div>
-        <header className="header-section">
-          <div className="header-top">
-            <div className="container">
-              <div className="ht-left">
-                <div className="mail-service">
-                  <i className=" fa fa-envelope"></i>
-                  202,Udhyog vihar phase V,Gurgaon(Haryana)
-                </div>
-                <div className="phone-service">
-                  <i className=" fa fa-phone"></i>
-                 9958065877
-                </div>
-              </div>
-              <div className="ht-right">
-                <NavLink
-                  exact
-                  className="login-panel"
-                  activeClassName="is-active"
-                  to="/Login"
-                >
-                  <i className="fa fa-user"></i>Admin Login
-                </NavLink>
+  //e.target.className="navvar-item active is-active ";
+  //e.target.backgroundColor="#E7AB3C";
+ };
 
-                <div className="top-social">
-                  <a href="https://www.facebook.com/modeZara6/">
-                    <i className="ti-facebook"></i>
-                  </a>
+ categorySelected(e) {
+  e.preventDefault();
+  console.log(e);
+  this.setState({
+   selectedOption: e.target.value,
+  });
+ }
 
-                  <a href="#">
-                    <i className="ti-linkedin"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="container">
-            <div className="inner-header">
-              <div className="row">
-                <div className="col-lg-2 col-md-2">
-                  <div className="logo">
+ render() {
+  const data = [
+   "Kurta",
+   "Kurta Plazo Set",
+   "A Line Kurta",
+   "Kurta Plazo Dupatta Set",
+  ];
+  let cats = data.map((ex) => {
+   return (
+    <option value={ex} key={ex} onClick={this.handleDropdown}>
+     {" "}
+     {ex}
+    </option>
+   );
+  });
+  return (
+   <div>
+    <header className="header-section">
+     <div className="header-top">
+      <div className="container">
+       <div className="ht-left">
+        <div className="mail-service">
+         <i className=" fa fa-envelope"></i>
+         202,Udhyog vihar phase V,Gurgaon(Haryana)
+        </div>
+        <div className="phone-service">
+         <i className=" fa fa-phone"></i>
+         9958065877
+        </div>
+       </div>
+       <div className="ht-right">
+        <NavLink
+         exact
+         className="login-panel"
+         activeClassName="is-active"
+         to="/Login"
+        >
+         <i className="fa fa-user"></i>Admin Login
+        </NavLink>
 
-			 <NavLink
+        <div className="top-social">
+         <a href="https://www.facebook.com/modeZara6/">
+          <i className="ti-facebook"></i>
+         </a>
+
+         <a href="#">
+          <i className="ti-linkedin"></i>
+         </a>
+        </div>
+       </div>
+      </div>
+     </div>
+     <div className="container">
+      <center>
+       <br />
+       <NavLink
+        exact
+        className="login-panel"
+        activeClassName="is-active"
+        to="/Home"
+       >
+        <img src="img/main_icon.jfif" />
+        <img src="img/main_logo.jfif" />
+       </NavLink>
+      </center>
+     </div>
+     <div className="container">
+      <div className="inner-header">
+       <div className="row">
+        <div className="col-lg-2 col-md-2">
+         <div className="logo">
+          {/* <NavLink
            exact
            className="login-panel"
            activeClassName="is-active"
            to="/Home"
           >
-           <img src="img/full_logo.jpeg" alt="" width="88" height="53" />
-          </NavLink>                   </div>
-                </div>
-                <div className="col-lg-7 col-md-7">
-                  <div className="advanced-search">
-                    {/* <select
+           <img src="img/logo_image.jpeg" />
+           <img src="img/logo_name.jpeg" />
+          </NavLink>{" "} */}
+         </div>
+        </div>
+        <div className="col-lg-7 col-md-7">
+         <div className="advanced-search">
+          {/* <select
                       placeholder="Choose category"
                       value={this.state.selectedOption}
                       onChange={this.categorySelected.bind(this)}
@@ -177,148 +192,144 @@ class Header extends React.Component {
                       </option>
                       {cats}
                     </select> */}
-                    <Dropdown>
-                      <Dropdown.Toggle
-                        variant="warning"
-                        id="dropdown-variants-warning"
-                        className="category-btn"
-                      >
-                        All categories
-                      </Dropdown.Toggle>
+          <Dropdown>
+           <Dropdown.Toggle
+            variant="warning"
+            id="dropdown-variants-warning"
+            className="category-btn"
+           >
+            All categories
+           </Dropdown.Toggle>
 
-                      <Dropdown.Menu>
-                        <Dropdown.Item href="/Kurta">Kurta</Dropdown.Item>
-                        <Dropdown.Item href="/KurtaPlazo">
-                          Kurta Plazo Set
-                        </Dropdown.Item>
-                        <Dropdown.Item href="/ALineKurta">
-                          A Line Kurta
-                        </Dropdown.Item>
-                        <Dropdown.Item href="/Dupatta">
-                          Kurta Plazo Dupatta Set
-                        </Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown> 
+           <Dropdown.Menu>
+            <Dropdown.Item href="/Kurta">Kurta</Dropdown.Item>
+            <Dropdown.Item href="/KurtaPlazo">Kurta Plazo Set</Dropdown.Item>
+            <Dropdown.Item href="/ALineKurta">A Line Kurta</Dropdown.Item>
+            <Dropdown.Item href="/Dupatta">
+             Kurta Plazo Dupatta Set
+            </Dropdown.Item>
+           </Dropdown.Menu>
+          </Dropdown>
 
-                    <div className="input-group">
-                      <input type="text" placeholder="What do you need?" />
-                      <button type="button">
-                        <i className="ti-search"></i>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-3 text-right col-md-3">
-                  <ul className="nav-right">
-                    <li className="heart-icon">
-                      {/* <a href="#">
+          <div className="input-group">
+           <input type="text" placeholder="What do you need?" />
+           <button type="button">
+            <i className="ti-search"></i>
+           </button>
+          </div>
+         </div>
+        </div>
+        <div className="col-lg-3 text-right col-md-3">
+         <ul className="nav-right">
+          <li className="heart-icon">
+           {/* <a href="#">
                         <label className="input">Your saved searches</label>
                         <i className="icon_heart_alt"></i>
                         <span>1</span>
                       </a> */}
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="nav-item">
-            <div className="container">
-              <nav className="nav-menu mobile-menu">
-                <ul>
-                  <li
-                    className={this.state.home ? "active" : null}
-                    onClick={this.handleClick}
-                  >
-                    <NavLink
-                      exact
-                      className="navvar-item"
-                      activeClassName="is-active"
-                      id="home"
-                      to="/Home"
-                    >
-                      Home
-                    </NavLink>
-                  </li>
-                  <li
-                    className={this.state.cat1 ? "active" : null}
-                    onClick={this.handleClick}
-                  >
-                    <NavLink
-                      exact
-                      className="navvar-item"
-                      activeClassName="is-active"
-                      id="cat1"
-                      to="/Kurta"
-                    >
-                      Kurta
-                    </NavLink>
-                  </li>
-                  <li
-                    className={this.state.cat2 ? "active" : null}
-                    onClick={this.handleClick}
-                  >
-                    <NavLink
-                      exact
-                      className="navvar-item"
-                      activeClassName="is-active"
-                      id="cat2"
-                      to="/KurtaPlazo"
-                    >
-                      Kurta Plazo
-                    </NavLink>
-                  </li>
-                  <li
-                    className={this.state.cat3 ? "active" : null}
-                    onClick={this.handleClick}
-                  >
-                    <NavLink
-                      exact
-                      className="navvar-item"
-                      activeClassName="is-active"
-                      id="cat3"
-                      to="/ALineKurta"
-                    >
-                      A Line Kurta
-                    </NavLink>
-                  </li>
-                  <li
-                    className={this.state.cat4 ? "active" : null}
-                    onClick={this.handleClick}
-                  >
-                    <NavLink
-                      exact
-                      className="navvar-item"
-                      activeClassName="is-active"
-                      id="cat4"
-                      to="/Dupatta"
-                    >
-                      Dupatta Set
-                    </NavLink>
-                  </li>
-                  <li
-                    className={this.state.contact ? "active" : null}
-                    onClick={this.handleClick}
-                  >
-                    <NavLink
-                      exact
-                      className="navvar-item"
-                      activeClassName="is-active"
-                      id="contact"
-                      to="/ContactUs"
-                    >
-                      Contact Us
-                    </NavLink>
-                  </li>
-                </ul>
-              </nav>
-              <div id="mobile-menu-wrap"></div>
-            </div>
-          </div>
-        </header>
+          </li>
+         </ul>
+        </div>
+       </div>
       </div>
-    );
-  }
+     </div>
+     <div className="nav-item">
+      <div className="container">
+       <nav className="nav-menu mobile-menu">
+        <ul>
+         <li
+          className={this.state.home ? "active" : null}
+          onClick={this.handleClick}
+         >
+          <NavLink
+           exact
+           className="navvar-item"
+           activeClassName="is-active"
+           id="home"
+           to="/Home"
+          >
+           Home
+          </NavLink>
+         </li>
+         <li
+          className={this.state.cat1 ? "active" : null}
+          onClick={this.handleClick}
+         >
+          <NavLink
+           exact
+           className="navvar-item"
+           activeClassName="is-active"
+           id="cat1"
+           to="/Kurta"
+          >
+           Kurta
+          </NavLink>
+         </li>
+         <li
+          className={this.state.cat2 ? "active" : null}
+          onClick={this.handleClick}
+         >
+          <NavLink
+           exact
+           className="navvar-item"
+           activeClassName="is-active"
+           id="cat2"
+           to="/KurtaPlazo"
+          >
+           Kurta Plazo
+          </NavLink>
+         </li>
+         <li
+          className={this.state.cat3 ? "active" : null}
+          onClick={this.handleClick}
+         >
+          <NavLink
+           exact
+           className="navvar-item"
+           activeClassName="is-active"
+           id="cat3"
+           to="/ALineKurta"
+          >
+           A Line Kurta
+          </NavLink>
+         </li>
+         <li
+          className={this.state.cat4 ? "active" : null}
+          onClick={this.handleClick}
+         >
+          <NavLink
+           exact
+           className="navvar-item"
+           activeClassName="is-active"
+           id="cat4"
+           to="/Dupatta"
+          >
+           Dupatta Set
+          </NavLink>
+         </li>
+         <li
+          className={this.state.contact ? "active" : null}
+          onClick={this.handleClick}
+         >
+          <NavLink
+           exact
+           className="navvar-item"
+           activeClassName="is-active"
+           id="contact"
+           to="/ContactUs"
+          >
+           Contact Us
+          </NavLink>
+         </li>
+        </ul>
+       </nav>
+       <div id="mobile-menu-wrap"></div>
+      </div>
+     </div>
+    </header>
+   </div>
+  );
+ }
 }
 
 export default Header;
